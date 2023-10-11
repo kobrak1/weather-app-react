cd3c58c247a780019d61c50ed8bfae048e28a455
kobrak1 <burakkarhan657@gmail.com>
2023-10-11 21:03:19 +0300

    bg settings modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState('Moscow');
+  const [location, setLocation] = useState('');
 
   const API_KEY = import.meta.env.VITE_API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
@@ -17,49 +17,69 @@ function App() {
       } catch (error) {
         console.error('Error while getting weather data:', error);
       }
+      setLocation('')
     }
   };
 
+  const getWeatherClass = () => {
+    const weatherCondition = data.weather?.[0]?.main;
+    switch (weatherCondition) {
+      case 'Rain':
+        return 'rain';
+      case 'Clear':
+        return 'clear';
+      case 'Snow':
+        return 'snow';
+      case 'Drizzle':
+        return 'drizzle';
+      case 'Thunderstorm':
+        return 'thunderstorm';
+      case 'Clouds':
+        return 'clouds';
+      case 'Haze':
+        return 'haze';
+  
+      default:
+        return 'default'; // Default class for unknown conditions
+    }
+  };
+
+  useEffect(() => {
+    // When the weather condition changes, apply the corresponding CSS class
+    const weatherClass = getWeatherClass();
+    document.body.className = weatherClass;
+  }, [data.weather]);
+
+  const icon = data.weather?.[0]?.icon
+  const weatherIconUrl = icon
+  ? `http://openweathermap.org/img/wn/${icon}@2x.png`
+  : '';
+
+  const mystyle = {
+    color: "white",
+    fontSize: "76px",
+    margin: "0",
+    paddingRight: "2rem"
+  };
   return (
     <div className="app">
       <div className="search">
         <input
           value={location}
           onChange={(event) => setLocation(event.target.value)}
-          onKeyDown={searchLocation} // Listen for the Enter key
+          onKeyDown={searchLocation} // it listens for the 'Enter' key"
           placeholder='Enter Location'
           type="text"
         />
       </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+      <div className="top-container">
+        <div className="temp">
+          {data.main ? <p style={mystyle}>{data.main.temp.toFixed()}°C</p> : null}
         </div>
-
-        {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        }
+        <div>
+          <img style={{paddingBottom:'.5rem'}} src={weatherIconUrl} width='50px'/>
+          <p style={{margin:'0', color:'white'}}> {data.weather?.[0]?.main} </p>
+        </div>
       </div>
     </div>
   );
